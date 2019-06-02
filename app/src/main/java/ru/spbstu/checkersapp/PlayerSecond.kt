0abfa8d9e20d6a3a8d04524079df7b669b2476f7
marlockd <denislogvinov42@gmail.com>
2019-06-02 16:38:48 +0300

    Added toolbar, other activities

---
+++ app/src/main/java/ru/spbstu/checkersapp/PlayerSecond.kt
@@ -0,0 +1,12 @@
+package ru.spbstu.checkersapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerSecond : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player_second)
+    }
+}
